0052ac2ae559846dd7fd4719b88d3dbc9d65dbc5
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-09-13 20:14:04 -0700

    Se quitó el scrollbar de toda la pagina

--- src/screens/chats/components/SideBar.jsx
+++ src/screens/chats/components/SideBar.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useCollection } from "react-firebase-hooks/firestore";
 import { collection } from "firebase/firestore";
 import { ArrowBack, ChatOutlined } from "@mui/icons-material"
-import { Box, Divider, Drawer, Grid, ListItemButton, List, ListItemIcon, ListItemText, Toolbar, Typography, IconButton } from "@mui/material"
+import { Box, Divider, Drawer, Grid, ListItemButton, List, ListItemIcon, ListItemText, Toolbar, Typography, IconButton, ListItemAvatar, Avatar } from "@mui/material"
 import { setActiveChat } from "../../../components/store/chats/thunks";
 import { db } from "../../../firebase/firebaseConfig";
 
@@ -51,20 +51,53 @@ const SideBar = ({ drawerWidth, displayMenu, setDisplayMenu }) => {
                 </Toolbar>
                 <Divider />
                 {
-                    <List>
+                    <List sx={{ width: '100%', maxWidth: 240, overflow:'hidden', scrollbarWidth: 'none', overflowY: 'scroll', }}>
                         {
                             chats?.filter(chat => chat.users.includes(email))
                                 .map(({ id, users }) => (
+                                    <>
+                                        <ListItemButton alignItems="flex-start" key={id} onClick={() => { handleClick(id, { username: displayUser(users) }) }}>
+                                            <ListItemAvatar>
+                                                <Avatar alt={displayUser(users)[0].toUpperCase()} src="{image}" />
+                                            </ListItemAvatar>
+                                            <ListItemText
+                                                primary={
+                                                    <Typography
+                                                        sx={{ display: 'inline' }}
+                                                        component="span"
+                                                        variant="body1"
+                                                        color="text.primary"
+                                                    >
+                                                        {displayUser(users)}
+                                                    </Typography>
+                                                }
+                                                secondary={
+                                                    <>
+                                                        <Typography
+                                                            sx={{ display: 'inline' }}
+                                                            component="span"
+                                                            variant="body2"
+                                                            color="text.primary"
+                                                        >
+                                                            LastSender
+                                                        </Typography>
+                                                        {" — Last message......"}
+                                                    </>
+                                                }
+                                            />
+                                        </ListItemButton>
+                                        <Divider variant="inset" component="li" />
+                                    </>
 
-                                    <ListItemButton key={id} onClick={() => { handleClick(id, { username: displayUser(users) }) }}>
-                                        <ListItemIcon>
-                                            <ChatOutlined />
-                                        </ListItemIcon> {/* Imagen de la persona del chat */}
-                                        <Grid container>
-                                            <ListItemText primary={displayUser(users)} />
-                                            {/* <ListItemText secondary={`${msg} ${time}`} /> */}
-                                        </Grid>
-                                    </ListItemButton>
+                                    // <ListItemButton key={id} onClick={() => { handleClick(id, { username: displayUser(users) }) }}>
+                                    //     <ListItemIcon>
+                                    //         <ChatOutlined />
+                                    //     </ListItemIcon> {/* Imagen de la persona del chat */}
+                                    //     <Grid container>
+                                    //         <ListItemText primary={displayUser(users)} />
+                                    //         {/* <ListItemText secondary={`${msg} ${time}`} /> */}
+                                    //     </Grid>
+                                    // </ListItemButton>
                                 ))
                         }
                     </List>
